c9ae11e87ae931ceee1bdbfa9f041e27a3b247a8
Kimi Code <kimi-code@example.com>
2026-08-05 16:42:34 +0800

    chore(inference): export LocalTransformersClient

--- src/qwen_stream_video/inference/__init__.py
+++ src/qwen_stream_video/inference/__init__.py
@@ -1,12 +1,13 @@
 """Inference components for qwen-stream-video."""
 
-from .client import FakeQwenClient, QwenClient, RawInferenceResult
+from .client import FakeQwenClient, LocalTransformersClient, QwenClient, RawInferenceResult
 from .parser import ResponseParser
 from .prompts import PromptBuilder
 from .validator import ObservationSemanticValidator
 
 __all__ = [
     "FakeQwenClient",
+    "LocalTransformersClient",
     "ObservationSemanticValidator",
     "PromptBuilder",
     "QwenClient",
